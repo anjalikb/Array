561f4f6c7f7f2cb361e2fc8ace1e5285085723f5
Anjali Gunjal <107025907+anjalikb@users.noreply.github.com>
2022-07-06 17:11:52 +0530

    Add files via upload

---
+++ array18.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class array18
+{
+public static void main(String args[])
+{
+  Scanner sc=new Scanner(System.in);
+   
+   
+   int count=0;
+   int i;
+   int[] a=new int[10];
+  System.out.println("enter the element");
+   for( i=0;i<10;i++)
+    a[i]=sc.nextInt();
+   
+   System.out.println("print the count of even no");
+   for(i=0 ; i<10 ; i++)
+  { 
+      if(a[i]%2==0)
+   {
+      System.out.println(a[i]+ " ");
+      count++;
+      
+    }
+  }
+     System.out.println("print the count of even no = " + count);
+     
+}
+}
